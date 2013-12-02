1ee0b65cab31f8a069112a6bee52377eca56f4d9
Rodolfo Campos <camposer@gmail.com>
2013-12-02 11:45:43 +0100

    Versión inicial

---
+++ TiendaWeb/src/es/indra/formacion/springmvc/basico/HolaMundoServlet.java
@@ -0,0 +1,50 @@
+package es.indra.formacion.springmvc.basico;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HolaMundoServlet
+ */
+@WebServlet("/HolaMundo")
+public class HolaMundoServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public HolaMundoServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nombre = request.getParameter("nombre");
+		
+		PrintWriter pw = response.getWriter();
+		pw.println("<html>");
+		pw.println("<head>");
+		pw.println("</head>");
+		pw.println("<body>");
+		pw.println("Hola: " + nombre);
+		pw.println("</body>");
+		pw.println("</html>");
+		pw.flush();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
